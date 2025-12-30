5532c36b722e70de3b325c59045235c7aab89ac8
LuDe <63415267+AliDerk@users.noreply.github.com>
2025-12-30 08:12:52 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 // Упрощенный вариант с Tabletop.js
 function initQueue() {
-    const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/ВАШ_ID/edit?usp=sharing';
+    const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/2PACX-1vS9GFUc83lUcJoHGqrgmWtSgkIy7LKvNfwXFQwnkC_yvcWqZVSS90tQRVQrPpZZp-PUNZw8hdUut_Oj/edit?usp=sharing';
     
     Tabletop.init({
         key: publicSpreadsheetUrl,
